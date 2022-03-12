79ab8415aa460976f4349ecf84bf43197b366ef0
Nicolas Serignese <nicoserig@gmail.com>
2022-03-12 00:37:21 -0300

    Agrego imagen adicional al logo

--- ecommerce-react/src/components/NavBar/NavBar.js
+++ ecommerce-react/src/components/NavBar/NavBar.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Button from '@mui/material/Button'
 import './NavBar.css'
 import Logo from './logo/logonb3.png'
-import { Home, Category, Info, AlternateEmail} from '@mui/icons-material';
+import LogoWRN from './logo/logowrn.png'
+import { Home, Category, Info, AlternateEmail } from '@mui/icons-material';
 
 function NavBar(){
     return(
         <header className="main-header">
             <img src={Logo} className={'navbar-logo'} alt='logo'/>
+            <img src={LogoWRN} className={'navbar-logo-wrn'} alt='logo wrn'/>
             <ul className = {'navbar'}>
                 <li><Button startIcon={<Home/>}>Home</Button></li>
                 <li><Button startIcon={<Category/>}>Productos</Button></li>
