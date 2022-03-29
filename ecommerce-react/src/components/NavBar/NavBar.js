d9bcc6f057f0f77c5cf25ba42b5491a2df90fcd5
Nicolas Serignese <nicoserig@gmail.com>
2022-03-29 20:18:39 -0300

    Agrego Links y saco estilo de link en botones.

--- ecommerce-react/src/components/NavBar/NavBar.js
+++ ecommerce-react/src/components/NavBar/NavBar.js
@@ -5,6 +5,7 @@ import './NavBar.css'
 import Logo from './logo/logonb3.png'
 import LogoWRN from './logo/logowrn.png'
 import { Home, Category, Info, AlternateEmail } from '@mui/icons-material';
+import { Link } from 'react-router-dom';
 
 function NavBar({inCart}){
     return(
@@ -12,8 +13,12 @@ function NavBar({inCart}){
             <img src={Logo} className={'navbar-logo'} alt='logo'/>
             <img src={LogoWRN} className={'navbar-logo-wrn'} alt='logo wrn'/>
             <ul className = {'navbar'}>
-                <li><Button startIcon={<Home/>}>Home</Button></li>
-                <li><Button startIcon={<Category/>}>Productos</Button></li>
+                <li><Button startIcon={<Home/>}>
+                        <Link to={'/'}>Home</Link>
+                    </Button></li>
+                <li><Button startIcon={<Category/>}>
+                        <Link to={'/productos'}>Productos</Link>
+                    </Button></li>
                 <li><Button startIcon={<Info/>}>Nosotros</Button></li>
                 <li><Button startIcon={<AlternateEmail/>}>Contacto</Button></li>
                 <li><CartWidget inCart={inCart}/></li>
