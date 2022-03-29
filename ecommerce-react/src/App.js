371a4966ace881a3e690dd11011f2f677bc212f0
Nicolas Serignese <nicoserig@gmail.com>
2022-03-29 19:48:01 -0300

    Implemento ruta producto y home dummy

--- ecommerce-react/src/App.js
+++ ecommerce-react/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
       <BrowserRouter>
         <NavBar inCart={0}/>
         <Routes>
-          <Route path='/' element={<h2>Home Page!</h2>}/>
-          <Route path='/productos' element={<HomePage />}/>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='/productos' element={<ProductsPage />}/>
         </Routes>
       </BrowserRouter>
     </div>
