374a47a4a1e04a7fd3f28fad3669e11046397014
Nicolas Serignese <nicoserig@gmail.com>
2022-04-02 20:00:49 -0300

    Termino Footer y modifico Home

--- ecommerce-react/src/components/Footer/Footer.js
+++ ecommerce-react/src/components/Footer/Footer.js
@@ -1,16 +1,33 @@
-import React from 'react'
-import Button from '@mui/material/Button'
+import React from 'react';
+
+// components
+import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+
+// icons
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import './Footer.css'
 
 function Footer(){
     return(
         <footer className="main-footer">
-            <ul className = 'footer'>
-                <li><Button > Instagram </Button></li>
-                <li><Button > Facebook </Button></li>
-                <li><Button > Nosotros </Button></li>
-                <li><Button > Contacto </Button></li>
-            </ul>
+            <div>
+                <p>Todos los derechos reservados - WRN 2022 ©</p>
+                <Stack direction="row" spacing={5} className='stack-style'>
+                    <IconButton aria-label="instagram" className='icon-btn'>
+                        <InstagramIcon />
+                    </IconButton>
+                    <IconButton aria-label="facebook" className='icon-btn'>
+                        <FacebookIcon />
+                    </IconButton>
+                    <IconButton aria-label="linkedin" className='icon-btn'>
+                        <LinkedInIcon />
+                    </IconButton>
+                </Stack>
+            </div>
         </footer>
     );
 }
