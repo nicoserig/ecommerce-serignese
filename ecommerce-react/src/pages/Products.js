7b4af7e43e1a619c6b14327ebfc681f561f83711
Nicolas Serignese <nicoserig@gmail.com>
2022-04-02 21:05:15 -0300

    Agrego onAddFx en Products

--- ecommerce-react/src/pages/Products.js
+++ ecommerce-react/src/pages/Products.js
@@ -4,12 +4,15 @@ import ItemListDetails from '../components/ItemListDetails/ItemListDetails';
 import ItemDetail from '../components/ItemDetail/ItemDetail'
 
 function ProductsPage({products}) {
-  
+  const onAddFx = () => {
+    alert("Add to cart - Placeholder")
+  }
+
   return (
     <div>
-      {console.log(products[0].author)}
+      {console.log(products[3].author)}
       {/* <ItemListContainer title={'¡Nuevos ingresos!'} allProducts={products}/> */}
-      <ItemDetail title={products[0].title} price={products[0].price} author={products[0].author} description={products[0].longDescription} stock={products[0].stock} prodImg={products[0].img}/>
+      <ItemDetail title={products[3].title} price={products[3].price} author={products[3].author} description={products[3].longDescription} stock={products[3].stock} prodImg={products[3].img} onAdd={onAddFx}/>
     </div>
   );
 }
