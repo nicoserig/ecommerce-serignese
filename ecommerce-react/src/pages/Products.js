73f89263212c544f037f2c1b9663413e4e448956
Nicolas Serignese <nicoserig@gmail.com>
2022-04-03 01:24:05 -0300

    Refactorizo Products

--- ecommerce-react/src/pages/Products.js
+++ ecommerce-react/src/pages/Products.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ItemListContainer from '../components/ItemListContainer/ItemListContainer'
 import ItemDetail from '../components/ItemDetail/ItemDetail'
 
 function ProductsPage({products}) {
@@ -9,8 +8,6 @@ function ProductsPage({products}) {
 
   return (
     <div>
-      {console.log(products[3].author)}
-      {/* <ItemListContainer title={'¡Nuevos ingresos!'} allProducts={products}/> */}
       <ItemDetail title={products[3].title} price={products[3].price} author={products[3].author} description={products[3].longDescription} stock={products[3].stock} prodImg={products[3].img} onAdd={onAddFx}/>
     </div>
   );
