198d2c27cfc7f26897645414d7ebfba9134f7d39
Nicolas Serignese <nicoserig@gmail.com>
2022-04-04 19:53:14 -0300

    Trabajo en página de contacto

--- ecommerce-react/src/pages/Contact.js
+++ ecommerce-react/src/pages/Contact.js
@@ -1,10 +1,60 @@
-import { FormControl } from '@mui/material';
+import { TextField, Container } from '@mui/material';
 import React from 'react'
+import './Contact.css'
 
 function ContactPage(){
-  return(
-      <p>Hola</p>
-  );
-}
+    return (
+        <Container maxWidth='xl'>
+            <div className='form-container'>
+                <div className='form-content-right'>
+                    <form className='form' noValidate>
+                        <h1>
+                        Completá nuestro formulario para recibir más información acerca de nuestros productos.
+                        </h1>
+                        <div className='form-inputs'>
+                        <label className='form-label'>Nombre</label>
+                        <input
+                            className='form-input'
+                            type='text'
+                            name='name'
+                            placeholder='Ingresá tu nombre'
+                        />
+                        </div>
+                        <div className='form-inputs'>
+                        <label className='form-label'>Email</label>
+                        <input
+                            className='form-input'
+                            type='email'
+                            name='email'
+                            placeholder='Ingresá tu email'
+                            />
+                        </div>
+                        <div className='form-inputs'>
+                            <label className='form-label'>Celular</label>
+                            <input
+                                className='form-input'
+                                type='number'
+                                name='celular'
+                                placeholder='Ingresá tu celular'
+                            />
+                        </div>
+                        <div className='form-inputs'>
+                            <label className='form-label'>Mensaje</label>
+                            <input
+                                className='form-input'
+                                type='text'
+                                name='mensaje'
+                                placeholder='Ingresá tu mensaje'
+                            />
+                        </div>
+                        <button className='form-input-btn' type='submit'>
+                        Enviar
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </Container>
+      );
+    };
 
 export default ContactPage;
